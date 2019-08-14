468d9855bc7c1fee54d251a32464605c5eb73539
Milind Patel <milind.patel@multidots.com>
2019-08-14 15:51:00 +0530

    First time 2.

--- app/src/main/java/com/example/myapplication/injection/module/NetworkModule.kt
+++ app/src/main/java/com/example/myapplication/injection/module/NetworkModule.kt
@@ -1,14 +1,24 @@
 package com.example.myapplication.injection.module
 
+import com.example.myapplication.BuildConfig
 import com.example.myapplication.network.PostApi
 import com.example.myapplication.utils.BASE_URL
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import okhttp3.logging.HttpLoggingInterceptor
+
+
 
 /**
  * Module which provides all required dependencies about network
@@ -17,6 +27,10 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 // Safe here as we are dealing with a Dagger 2 module
 @Suppress("unused")
 object NetworkModule {
+
+    const val HTTP_CONNECT_TIMEOUT = 60 * 1000
+    const val HTTP_READ_TIMEOUT = 60 * 1000
+
     /**
      * Provides the Post service implementation.
      * @param retrofit the Retrofit object used to instantiate the service
@@ -38,11 +52,36 @@ object NetworkModule {
     @JvmStatic
     internal fun provideRetrofitInterface(): Retrofit {
 
-        return Retrofit.Builder()
+       /* return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
-                .build()
+                .build()*/
+
+
+        val gson: Gson = with(GsonBuilder()) {
+            setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            create()
+        }
+
+
+        val builder = OkHttpClient.Builder()
+        builder.connectTimeout(HTTP_CONNECT_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        builder.readTimeout(HTTP_READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
+        builder.writeTimeout(HTTP_READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
+
+        val interceptor =  HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            builder.addInterceptor(interceptor)
+        }
+
+        return  Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(builder.build())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
 
     }
 }
